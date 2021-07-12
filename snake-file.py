7bc7cee356e5c916e933f44f56c93b3e812a250d
JeremiahHarris19 <75698932+JeremiahHarris19@users.noreply.github.com>
2021-07-12 11:59:21 -0400

    Update snake-file.py

--- snake-file.py
+++ snake-file.py
@@ -75,6 +75,12 @@ def gameLoop():
     x1_change = 0
     y1_change = 0
 
+    x2 = dis_width / 2
+    y2 = dis_width / 2
+
+    x2_change = 0
+    y2_change = 0
+
     snake_List = []
     Length_of_snake = 1
 
@@ -121,33 +127,39 @@ def gameLoop():
                     x1_change = 0
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    x1_change = -snake_block
-                    y1_change = 0
+                    x2_change = -snake_block
+                    y2_change = 0
                 elif event.key == pygame.K_d:
-                    x1_change = snake_block
-                    y1_change = 0
+                    x2_change = snake_block
+                    y2_change = 0
                 elif event.key == pygame.K_w:
-                    y1_change = -snake_block
-                    x1_change = 0
+                    y2_change = -snake_block
+                    x2_change = 0
                 elif event.key == pygame.K_s:
-                    y1_change = snake_block
-                    x1_change = 0
+                    y2_change = snake_block
+                    x2_change = 0
 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0 or x1 >= dis_width or x2 < 0 or y1 >= dis_height or y2 < 0:
             game_close = True
         x1 += x1_change
         y1 += y1_change
+        x2 += x2_change
+        y2 += y2_change
         dis.fill(blue)
         pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
-        snake_Head = []
-        snake_Head.append(x1)
-        snake_Head.append(y1)
-        snake_List.append(snake_Head)
+        snake_Head1 = []
+        snake_Head2 = []
+        snake_Head1.append(x1)
+        snake_Head1.append(y1)
+        snake_Head2.append(x2)
+        snake_Head2.append(y2)
+        snake_List.append(snake_Head1)
+        snake_List.append(snake_Head2)
         if len(snake_List) > Length_of_snake:
             del snake_List[0]
 
         for x in snake_List[:-1]:
-            if x == snake_Head:
+            if x == snake_Head1:
                 game_close = True
 
         snake_1(snake_block, snake_List)
